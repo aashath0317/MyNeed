64d82b42bfabf66de3ab4ada9a4678f074dcec9a
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2022-01-25 16:34:36 +0530

    Update wserver.py

--- wserver.py
+++ wserver.py
@@ -204,13 +204,13 @@ input[type="submit"]:hover, input[type="submit"]:focus{
           src="https://telegra.ph/file/cc06d0c613491080cc174.png"
           alt="logo"
         />
-        <a href="https://t.me/anas_tayyar">
+       <a href="https://t.me/aashath">
           <h2 class="name">Qbittorrent Selection</h2>
         </a>
       </div>
       <div class="social">
-        <a href="https://www.github.com/anasty17/mirror-leech-telegram-bot"><i class="fab fa-github"></i></a>
-        <a href="https://t.me/anas_tayyar"><i class="fab fa-telegram"></i></a>
+        <a href="https://c2ptech.com"><i class="fab fa-website"></i></a>
+        <a href="https://t.me/aashath"><i class="fab fa-telegram"></i></a>
       </div>
     </header>
     <section>
@@ -579,13 +579,13 @@ section span{
           src="https://telegra.ph/file/cc06d0c613491080cc174.png"
           alt="logo"
         />
-        <a href="https://t.me/anas_tayyar">
+        <a href="https://t.me/aashath">
           <h2 class="name">Qbittorrent Selection</h2>
         </a>
       </div>
       <div class="social">
-        <a href="https://www.github.com/anasty17/mirror-leech-telegram-bot"><i class="fab fa-github"></i></a>
-        <a href="https://t.me/anas_tayyar"><i class="fab fa-telegram"></i></a>
+        <a href="https://c2ptech.com"><i class="fab fa-website"></i></a>
+        <a href="https://t.me/aashath"><i class="fab fa-telegram"></i></a>
       </div>
     </header>
     <section>
@@ -735,7 +735,7 @@ async def set_priority(request):
 @routes.get('/')
 async def homepage(request):
 
-    return web.Response(text="<h1>See mirror-leech-telegram-bot <a href='https://www.github.com/anasty17/mirror-leech-telegram-bot'>@GitHub</a> By <a href='https://github.com/anasty17'>Anas</a></h1>", content_type="text/html")
+    return web.Response(text="<h1>c2ptech_bot <a href='https://www.t.me/aashath'>@Telegram</a> By <a href='https://www.t.me/aashath'>Aashath</a></h1>", content_type="text/html")
 
 async def e404_middleware(app, handler):
 
@@ -744,11 +744,11 @@ async def e404_middleware(app, handler):
         try:
             response = await handler(request)
             if response.status == 404:
-                return web.Response(text="<h1>404: Page not found</h2><br><h3>mirror-leech-telegram-bot</h3>", content_type="text/html")
+                return web.Response(text="<h1>404: Page not found</h2><br><h3>c2ptech_bot</h3>", content_type="text/html")
             return response
         except web.HTTPException as ex:
             if ex.status == 404:
-                return web.Response(text="<h1>404: Page not found</h2><br><h3>mirror-leech-telegram-bot</h3>", content_type="text/html")
+               return web.Response(text="<h1>404: Page not found</h2><br><h3>c2ptech_bot</h3>", content_type="text/html")
             raise
     return middleware_handler
 
