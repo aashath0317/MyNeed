e6b91578a8e1ef9c0cd9cbc9e73c844b09c8b4b6
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2022-01-25 16:36:42 +0530

    Update telegraph_helper.py

--- telegraph_helper.py
+++ telegraph_helper.py
@@ -57,4 +57,4 @@ class TelegraphHelper:
 			return self.edit_page(path, title, content)
 
 
-telegraph=TelegraphHelper('Mirror-Leech-Telegram-Bot', 'https://github.com/anasty17/mirror-leech-telegram-bot')
+telegraph=TelegraphHelper('C2PTECH_BOT', 'https://c2ptech.com')
