f18e9e78841d81bf4cb300998efda0ab891d44dc
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2022-01-25 16:44:57 +0530

    Update gdriveTools.py

--- gdriveTools.py
+++ gdriveTools.py
@@ -168,7 +168,7 @@ class GoogleDriveHelper:
         # File body description
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded by Mirror-leech-telegram-bot',
+            'description': 'Uploaded by C2PTECH Bot',
             'mimeType': mime_type,
         }
         if parent_id is not None:
@@ -436,7 +436,7 @@ class GoogleDriveHelper:
     def __create_directory(self, directory_name, parent_id):
         file_metadata = {
             "name": directory_name,
-            "description": "Uploaded by Mirror-leech-telegram-bot",
+            "description": "Uploaded by C2PTECH Bot",
             "mimeType": self.__G_DRIVE_DIR_MIME_TYPE
         }
         if parent_id is not None:
@@ -721,7 +721,7 @@ class GoogleDriveHelper:
         for content in self.telegraph_content:
             self.path.append(
                 telegraph.create_page(
-                    title='Mirror-Leech-Bot Drive Search',
+                    title='C2PTECH Drive Search',
                     content=content
                 )["path"]
             )
